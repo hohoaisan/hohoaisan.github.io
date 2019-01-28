6f8396449d78be0d414407e575dfe869f49305fa
hohoaisan <hohoaisan@gmail.com>
2019-01-28 17:55:58 +0700

    Recontruct database and jQuery table view

--- docs/content/footable.js
+++ docs/content/footable.js
@@ -15,57 +15,10 @@ jQuery(function ($) {
 			{ "name": "name", "title": "Tên" },
 			{ "name": "group", "title": "Nhóm" },
 			{ "name": "type", "title": "Kiểu" },
-			{ "name": "status", "title": "✓" },
 			{ "name": "ep", "title": "Số tập", "type": "text" },
-			{
-				"name": "1080p1",
-				"title": "FHD",
-				"formatter": function (value) {
-					if (value != '') { value = '<a href="' + value + '" target="_blank"> FHD </a>'; }
-					return value;
-				}
-			},
-			{
-				"name": "1080p2",
-				"title": "FHD",
-				"formatter": function (value) {
-					if (value != '') { value = '<a href="' + value + '" target="_blank"> FHD </a>'; }
-					return value;
-				}
-			},
-			{
-				"name": "720p1",
-				"title": "HD",
-				"formatter": function (value) {
-					if (value != '') { value = '<a href="' + value + '" target="_blank"> HD </a>'; }
-					return value;
-				}
-			},
-			{
-				"name": "720p2",
-				"title": "HD",
-				"formatter": function (value) {
-					if (value != '') { value = '<a href="' + value + '" target="_blank"> HD </a>'; }
-					return value;
-				}
-			},
-			{
-				"name": "480p1",
-				"title": "SD",
-				"formatter": function (value) {
-					if (value != '') {
-						value = '<a href="' + value + '" target="_blank"> SD </a>';
-					}
-					return value;
-				}
-			},
-			{
-				"name": "480p2",
-				"title": "SD",
-				"formatter": function (value) {
-					if (value != '') { value = '<a href="' + value + '" target="_blank"> SD </a>'; }
-					return value;
-				}
+			{ "name": "1080p",	"title": "FHD", "type": "link"},
+			{ "name": "720p",	"title": "HD", "type": "link"},
+			{ "name": "480p",	"title": "SD", "type": "link"
 			}],
 
 		"rows": $.get("docs/content/rows.json")
