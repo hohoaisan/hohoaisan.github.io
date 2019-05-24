d6737dde198a2a6be86dfc970c0befa33c744bdf
hohoaisan <hohoaisan@gmail.com>
2019-05-24 20:56:50 +0700

    updated comment

--- web/menu.js
+++ web/menu.js
@@ -1,6 +1,6 @@
     //Điều khiển nút Menu, khi cửa sổ thay đổi thì nếu dropdown-content đang được xổ xuống thì phải tắt nó đi
-    //Khi cửa sổ nhỏ hơn giới hạn wmaxsize thì lập tức xoá class container của #navmenu để nút #menu và .search nằm dàn ra sát màn hình
-    var wmaxsize = 895;
+    //Khi cửa sổ nhỏ hơn giới hạn wmaxsize thì lập tức xoá class container của div#navmenu để nút div#menu và div.right-menu nằm dàn ra sát màn hình
+    var wmaxsize = 800;
     var menu = document.getElementById("menu")
     var x = document.getElementById("dropdown-content")
     if (window.innerWidth <= wmaxsize) {
@@ -17,13 +17,13 @@
             document.getElementById("navmenu").classList.add("container")
         }
     };
-    //Khi click vào nút #menu thì dropdown-content sẽ được xổ xuống, nếu click lại nó hoặc click ra bên ngoài thì nó sẽ tự tắt đi
+    //Khi click vào một phần tử bất kì trong web, nếu phần tử đó có id là #menu hoặc class là menu-img thì dropdown-content sẽ được xổ xuống, nếu click lại chính nó hoặc click ra bên ngoài thì dropdown-content tắt đi
     document.onclick = function toggle_dropdown() {
         var w = window.innerWidth;
         a = window.event.srcElement.id;
         b = window.event.srcElement.className;
         if (w <= wmaxsize) {
-            if (a === "menu" || b === "menu-border") {
+            if (a === "menu" || b === "menu-img") {
                 if (x.style.display === "none") {
                     x.style.display = "block";
                 } else {
